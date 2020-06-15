ade50793d7d4a1f32fbeebc15ef166e961778743
Vasanth Premala <53056749+VasanthPremala@users.noreply.github.com>
2020-06-15 21:27:49 +0530

    Update Cyber_Security_Data_Encryption_Code.py

--- Cyber_Security_Data_Encryption_Code.py
+++ Cyber_Security_Data_Encryption_Code.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from Crypto import Random
 from Crypto.Cipher import AES
 import os
@@ -197,12 +191,3 @@ if __name__ == '__main__':
             break
         else:
             print("Enter proper choice")
-
-       
-
-
-# In[ ]:
-
-
-
-
